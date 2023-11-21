658c59d6d20199cbd3dfe916ecc91136ad8475c3
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2023-11-21 00:51:59 -0500

    Add hero images and villain images to results section

--- src/pages/Figths/Figth.jsx
+++ src/pages/Figths/Figth.jsx
@@ -37,6 +37,10 @@ function Figth() {
   const [mostFrequentVillain, setMostFrequentVillain] = useState(null);
   const [topHeroes, setTopHeroes] = useState([]);
   const [mostDefeatedVillainByTeenHero, setMostDefeatedVillainByTeenHero] = useState(null);
+  const [selectedHeroImage, setSelectedHeroImage] = useState(null);
+  const [mostFrequentVillainImage, setMostFrequentVillainImage] = useState(null);
+  const [topHeroesImages, setTopHeroesImages] = useState([]);
+  const [mostDefeatedVillainImage, setMostDefeatedVillainImage] = useState(null);
 
   const findMostFrequentVillain = (heroName) => {
     const heroFights = fights.filter((fight) => fight.hero === heroName);
@@ -99,34 +103,78 @@ function Figth() {
 
     const defeatedVillain = findMostDefeatedVillainByTeenHero();
     setMostDefeatedVillainByTeenHero(defeatedVillain);
-  }, [selectedHero]);
+
+    const selectedHeroData = characters.find((hero) => hero.alias === selectedHero);
+    if (selectedHeroData) {
+      setSelectedHeroImage(selectedHeroData.picture);
+    }
+
+    const frequentVillainData = characters.find((villain) => villain.alias === frequentVillain);
+    if (frequentVillainData) {
+      setMostFrequentVillainImage(frequentVillainData.picture);
+    }
+
+    // Find images for the top 3 heroes
+    const topHeroesImagesData = heroes.map((hero) => {
+      const heroData = characters.find((char) => char.alias === hero.name);
+      return heroData ? heroData.picture : null;
+    });
+    setTopHeroesImages(topHeroesImagesData);
+
+    // Find image for the most defeated villain
+    const mostDefeatedVillainData = characters.find((villain) => villain.alias === defeatedVillain);
+    if (mostDefeatedVillainData) {
+      setMostDefeatedVillainImage(mostDefeatedVillainData.picture);
+    }
+  }, [selectedHero, characters]);
 
   return (
     <>
       <MyContext.Provider value={characters}>
         <Navbar />
-        <div>
-            <label htmlFor="heroSelect">Select a hero: </label>
+        <div className='results'>
+          <div className='selectHeroe'>
+            <label className='heroLabel' htmlFor="heroSelect">Select a hero: </label>
             <select className='heroSelect' id="heroSelect" onChange={(e) => setSelectedHero(e.target.value)} value={selectedHero}>
-                {characters
+              {characters
                 .filter((hero) => hero.rol === 'Heroe')
                 .map((hero) => (
-                    <option key={hero.alias} value={hero.alias}>
+                  <option key={hero.alias} value={hero.alias}>
                     {hero.alias}
-                    </option>
+                  </option>
                 ))}
             </select>
-            <h2>Results:</h2>
+          </div>
+
+          <h2>Results:</h2>
+          <div className='mostFrequentFigth'>
+            {selectedHeroImage && (
+              <img src={selectedHeroImage} alt={`${selectedHero}'s Image`} className="heroImage" />
+            )}
             <p>1. Villain fought most by {selectedHero}: {mostFrequentVillain}</p>
-            <h3>2. Top 3 heroes with the most victories:</h3>
-            <ul>
-                {topHeroes.map((hero, index) => (
-                <li key={index}>
+            {mostFrequentVillainImage && (
+              <img src={mostFrequentVillainImage} alt={`${mostFrequentVillain}'s Image`} className="villainImage" />
+            )}
+          </div>
+          <h3>2. Top 3 heroes with the most victories:</h3>
+          <ul>
+            {topHeroes.map((hero, index) => (
+              <li key={index}>
                 {hero.name}: {hero.victories} victories
-                </li>
-                ))}
-            </ul>
-            <p>3. Villain lost most against a teen hero: {mostDefeatedVillainByTeenHero}</p>
+              </li>
+            ))}
+          </ul>
+          <div className='topHeroesImages'>
+            {topHeroesImages.map((image, index) => (
+              <img key={index} src={image} alt={`Top Hero ${index + 1}`} className="topHeroImage" />
+            ))}
+          </div>
+          <p>3. Villain lost most against a teen hero: {mostDefeatedVillainByTeenHero}</p>
+          {mostDefeatedVillainImage && (
+            <div className='mostDefeatedVillain'>
+              <img src={mostDefeatedVillainImage} alt={`${mostDefeatedVillainByTeenHero}'s Image`} className="mostDefeatedVillainImage" />
+            </div>
+          )}
         </div>
 
       </MyContext.Provider>
